f3d952d294da2fea2646f35f808d3f73743dafa7
Marcos Salles <dev.salles7@gmail.com>
2026-03-04 19:18:46 -0300

    Data de projetos e tecnologias

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        num: '001',
+        title: 'Projeto 1',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        tags: ['Vue.js', 'Vue.js', 'Vue.js'],
+        link: '#',
+    },
+    {
+        num: '002',
+        title: 'Projeto 2',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        tags: ['Vue.js', 'Vue.js', 'Vue.js'],
+        link: '#',
+    },
+    {
+        num: '003',
+        title: 'Projeto 3',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        tags: ['Vue.js', 'Vue.js', 'Vue.js'],
+        link: '#',
+    },
+    {
+        num: '004',
+        title: 'Projeto X',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        tags: ['Vue.js', 'Vue.js', 'Vue.js'],
+        link: '#',
+    },
+]
---
+++ src/data/techs.js
@@ -0,0 +1,9 @@
+export const techs = [
+    { icon: '', name: 'Java', category: 'Backend' },
+    { icon: '', name: 'Vue.js', category: 'Frontend' },
+    { icon: '', name: 'TypeScript', category: 'Linguagem' },
+    { icon: '', name: 'PostgreSQL', category: 'Banco de dados' },
+    { icon: '', name: 'Docker', category: 'Infra' },
+    { icon: '', name: 'Tailwind', category: 'Estilo' },
+    { icon: '', name: 'Git', category: 'Versionamento' },
+]
